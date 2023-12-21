f5bf4f344c8e43793d7f6bc0e8ef5f0489a2c9f2
Mladen Rabasovic <mrabasovic@Mladens-MacBook-Air.local>
2023-12-21 17:41:26 +0100

    started project, added nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import Home from './screens/Home/Home.js';
+// import About from './screens/AboutUs/About';
+// import Contact from './screens/Contact/Contact';
+// import Login from './screens/Login/Login';
+// import Register from './screens/Register/Register';
+import NavBar from './components/NavBar';
+// import OurTravelers from './screens/OurTravelers';
+// import DestinationDetails from './screens/DestinationDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <div>
+        <NavBar className="navBar"/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/our-travelers" element={<OurTravelers />} />
+          <Route path="/destination-details" element={<DestinationDetails />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/NavBar/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import LogoImagePath from '../../assets/logo.png';
+import './NavBar.css';
+import { FaBars } from "react-icons/fa";
+import { NavLink as Link } from "react-router-dom";
+
+const NavBar = () => {
+	return (
+		<>
+			<nav className="Nav">
+				<FaBars className="Bars" />
+				<div className="NavMenu">
+					<img className="LogoImage" src={LogoImagePath} alt="Logo" height={'150px'} width={'150px'}/>
+					<Link to="/" className="NavLink">
+						Home
+					</Link>
+					<Link to="/About" className="NavLink" activeClassName="active">
+						About us
+					</Link>
+					<Link to="/Contact" className="NavLink" activeClassName="active">
+						Contact
+					</Link>
+				</div>
+				<div className="NavBtn">
+					<Link to="/login" className="NavBtnLink">
+						Sign In
+					</Link>
+					<Link to="/register" className="NavBtnLink">
+						Sign Up
+					</Link>
+				</div>
+			</nav>
+		</>
+	);
+};
+
+export default NavBar;
